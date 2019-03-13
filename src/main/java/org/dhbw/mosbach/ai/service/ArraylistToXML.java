55439f4b390f32f46ca7ec6a61fbf1f57632c77b
Oliver <oliver.rottweil@gmail.com>
2019-03-13 11:57:59 +0100

    ArrayList zu JSON zu XML

---
+++ src/main/java/org/dhbw/mosbach/ai/service/ArraylistToXML.java
@@ -0,0 +1,28 @@
+package org.dhbw.mosbach.ai.service;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class ArraylistToXML {
+
+    public JSONObject arraylistToJSON(ArrayList<ArrayList<ArrayList<String>>> theList){
+        JSONObject theResult = new JSONObject();
+        int recipeNumber=0;
+        for(int i=0;i<theList.size();i++) {//für jedes Rezept ein JSON-Objekt
+            if(theList.get(i).size()!=0) {
+                JSONObject tempResult = new JSONObject();
+                tempResult.put("name",theList.get(i).get(2).get(0));
+                for (int j = 0; j < theList.get(i).get(0).size(); j++) {
+                    tempResult.put(theList.get(i).get(0).get(j), theList.get(i).get(1).get(j));
+                }
+                tempResult.put("recipe", theList.get(i).get(3).get(0));
+                theResult.put("Recipe"+recipeNumber, tempResult);
+                recipeNumber++;
+            }
+        }
+        System.out.println();
+        return  theResult;
+    }
+
+}
--- src/main/java/org/dhbw/mosbach/ai/service/Main.java
+++ src/main/java/org/dhbw/mosbach/ai/service/Main.java
@@ -1,14 +1,22 @@
 package org.dhbw.mosbach.ai.service;
 
+import org.json.JSONObject;
+import org.json.XML;
+
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[]args){
         RecipeGetter dataGetter = new RecipeGetter();
         ArrayList<ArrayList<ArrayList<String>>> theFoodList=dataGetter.getDataFromAPIFood("Lasagne,Szechuan Beef,Pork Cassoulet,sdfsgsd,Spaghetti");
-        ArrayList<ArrayList<ArrayList<String>>> theDrinkList=dataGetter.getDataFromAPIDrinks("Vodka,");
+        //KANN MEHRERE ERGEBNISSE PRO ESSEN/TRINKEN GEBEN!!
+        ArrayList<ArrayList<ArrayList<String>>> theDrinkList=dataGetter.getDataFromAPIDrinks("Vodka");
         IngridientCalculator calculator = new IngridientCalculator();
         calculator.calculateAmounts(theFoodList);
-        System.out.println();
+        ArraylistToXML theXMLMaker = new ArraylistToXML();
+        JSONObject theJSON = theXMLMaker.arraylistToJSON(theFoodList);
+        String xml = XML.toString(theJSON);
+        System.out.println(xml);
+
     }
 }
