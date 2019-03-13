3006e30d873a9fa7e344a7f16afa6ff13a7114b0
Oliver <oliver.rottweil@gmail.com>
2019-03-13 09:24:37 +0100

    getFood und getDrinks anstatt alles in einem

--- src/main/java/org/dhbw/mosbach/ai/service/Main.java
+++ src/main/java/org/dhbw/mosbach/ai/service/Main.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[]args){
         getData dataGetter = new getData();
-        ArrayList<ArrayList<ArrayList<ArrayList<String>>>> theFoodList=dataGetter.getDataFromAPI("Lasagne,,sdfsgsd,Spaghetti","");
+        ArrayList<ArrayList<ArrayList<String>>> theFoodList=dataGetter.getDataFromAPIFood("Lasagne,,sdfsgsd,Spaghetti");
+        ArrayList<ArrayList<ArrayList<String>>> theDrinkList=dataGetter.getDataFromAPIDrinks("Vodka");
         System.out.println();
     }
 }
